681ee184f0f9c5f4538bdf96b9454411fe821bf1
countkrusty <tristan.wiessalla@posteo.de>
2020-04-17 12:30:12 +0200

    unambiguous recognition of files to read

--- C_clusterDistances.m
+++ C_clusterDistances.m
@@ -1,12 +1,12 @@
 function C_clusterDistances(output_folder)
-cossim_files = dir(strcat(output_folder,'/*.mat'));
-[~, idx] = sort([cossim_files.datenum]);
-cossim_files = cossim_files(idx);
-cossim_files = {cossim_files.name};
+boot_files = dir(strcat(output_folder, 'Avg*'));
+dist_files = dir(strcat(output_folder, 'Cos*'));
+boot_files = {boot_files.name};
+dist_files = {dist_files.name};
 
-for vidOutput=1:2:length(cossim_files)
-    boot = load(cossim_files{vidOutput});
-    dist = load(cossim_files{vidOutput + 1});
+for vidOutput=1:length(dist_files)
+    boot = load(boot_files{vidOutput});
+    dist = load(dist_files{vidOutput});
     dist = dist.cossim_hogs;
     cutoff = quantile(boot.avg_distance, 0.95);
     stereotypedFrames = extractStereotyped(dist, cutoff);
