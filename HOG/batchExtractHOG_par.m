a595415510ac614f045804a3b6e655466744f970
countkrusty <tristan.wiessalla@posteo.de>
2020-04-25 13:40:08 +0200

    typos

--- HOG/batchExtractHOG_par.m
+++ HOG/batchExtractHOG_par.m
@@ -72,17 +72,7 @@ function [number_dataChunks, numFrames] = separateVideo(vidReader, size_dataChun
         frame1 = grayCrop(frame1, cropRangeY, cropRangeX);
         hog_vec = extractHOGFeatures(frame1, 'BlockSize', [32 32], 'NumBins', 8, ...
                             'BlockSize', [1 1]);
-        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        hog1_byteSize = whos('hog_vec').bytes;
+        hog1_byteSize = whos('hog_vec').bytes;
         numFrames = length(laserSwitchOn_idx:laserSwitchOff_idx);
         estimated_byteSize = numFrames*hog1_byteSize;
         number_dataChunks = ceil(estimated_byteSize/size_dataChunks);
