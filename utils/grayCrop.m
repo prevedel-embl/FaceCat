076f64a27f080c0a18886a4bbced736b7e523b53
countkrusty <tristan.wiessalla@posteo.de>
2020-04-29 18:11:02 +0200

    fixed inversion of crop range x/y

--- utils/grayCrop.m
+++ utils/grayCrop.m
@@ -2,6 +2,7 @@ function frame = grayCrop(frame, pos_crop)
     if ndims(frame) > 2
                 frame = rgb2gray(frame);
     end
-    frame = frame(pos_crop(1):pos_crop(1) + pos_crop(3), ...
-                        pos_crop(2):pos_crop(2) + pos_crop(4));
+    pos_crop = floor(pos_crop);
+    frame = frame(pos_crop(2):pos_crop(2) + pos_crop(4), ...
+                    pos_crop(1):pos_crop(1) + pos_crop(3));
 end
