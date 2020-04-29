b377ebc1d42b2f7a963cfe0ed4f859fca2b30e77
countkrusty <tristan.wiessalla@posteo.de>
2020-04-29 12:39:01 +0200

    adjusted crop range

--- utils/grayCrop.m
+++ utils/grayCrop.m
@@ -2,5 +2,6 @@ function frame = grayCrop(frame, pos_crop)
     if ndims(frame) > 2
                 frame = rgb2gray(frame);
     end
-    frame = frame(pos_crop(1):pos_crop(3), pos_crop(2):pos_crop(4));
+    frame = frame(pos_crop(1):pos_crop(1) + pos_crop(3), ...
+                        pos_crop(2):pos_crop(2) + pos_crop(4));
 end
