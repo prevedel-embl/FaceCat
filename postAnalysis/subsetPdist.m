78984a119cf453c3f0640f08f609827e10629f07
countkrusty <tristan.wiessalla@posteo.de>
2020-05-06 11:10:32 +0200

    initial commit

--- postAnalysis/subsetPdist.m
+++ postAnalysis/subsetPdist.m
@@ -2,14 +2,15 @@ function subsetDistMat = subsetPdist(framesStart, framesEnd, len, distMat)
 %% Extract subset from a distance matrix, using the original indices and the total number of comparisons underlying the matrix
     subsetDistMat = [];
     segmentLen = framesEnd - framesStart;
-    for i = framesStart:framesEnd
+    for i = 1:segmentLen
         if i == 1
-            lowerIndex = 1;
-            higherIndex = segmentLen;
+            lowerIndex = framesStart;
+            higherIndex = framesEnd - 1;
         else
             lowerIndex = higherIndex + len - segmentLen;
             higherIndex = lowerIndex + segmentLen - i;
         end
         subsetDistMat = [subsetDistMat, distMat(lowerIndex:higherIndex)];
     end
+    subsetDistMat = squareform(subsetDistMat);
 end
