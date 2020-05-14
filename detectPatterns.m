03ea1f3c721fe9a3f8a3ccf3cdd481f6a93c670b
countkrusty <tristan.wiessalla@posteo.de>
2020-05-14 13:34:23 +0200

    convolutional approach, struct output

--- detectPatterns.m
+++ detectPatterns.m
@@ -1,12 +1,20 @@
-
-function patComp = detectPatterns(stereotypedFrames)
-    for i=1:length(stereotypedFrames)-15
-        a(i, :) = stereotypedFrames(i:i+14, 1);
+function patComp = detectPatterns(stereotypedFrames, windowSize)
+    for i=1:length(stereotypedFrames)-windowSize
+        allPatterns(i, :) = stereotypedFrames(i:i+windowSize - 1, 1);
     end
     % Remove duplicate patterns to improve speed of the convolution
-    a = unique(a, 'rows', 'stable');
-    for i=1:length(a)
-        patComp(i,:) = conv(stereotypedFrames, a(i,:), 'same');
-        patComp(i,:) = patComp(i,:)/max(patComp(i,:));
+    allPatterns = unique(allPatterns, 'rows', 'stable');
+    patComp = struct('Score', {}, 'Overlap_Locations', {}, 'Count', {});
+    for i=1:size(allPatterns,1)
+        pattern = allPatterns(i, :);  
+        tmp = colfilt(stereotypedFrames, [15 1], 'sliding', @(x) overlapCount(x, pattern'));
+        patComp(i).Score = tmp(:,1);
+        patComp(i).Overlap_Locations = find(tmp(:,1) == 15);
+        patComp(i).Count = length(find(tmp(:,1) == 15));
     end
+
 end
+
+function out = overlapCount(block, pattern)
+    out = sum(block == pattern);
+end    
