a7eea7bd3e8a007ca27312423027dd823b4b5f28
countkrusty <tristan.wiessalla@posteo.de>
2020-05-18 16:13:29 +0200

    finally correct frame count

--- HOG/batchExtractHOG_concat.m
+++ HOG/batchExtractHOG_concat.m
@@ -1,4 +1,4 @@
-function batchExtractHOG_par(video_path, laserSwitchOn_idcs, laserSwitchOff_idcs, ...
+function batchExtractHOG_concat(video_path, laserSwitchOn_idcs, laserSwitchOff_idcs, ...
             batchNum, pos_snout)
     vidReader = VideoReader(video_path);
     % Extract File Name, for savename generation
@@ -10,7 +10,8 @@ function batchExtractHOG_par(video_path, laserSwitchOn_idcs, laserSwitchOff_idcs
         keyboard
     end
     dataChunks = length(laserSwitchOn_idcs);
-    % Load only these chunks of 2GB size into memory, one at a time
+    % Concatenate the indices of all frames during which the Ca recording
+    % laser is on
     recordedFrames = [];
     for N=1:dataChunks
         tmp = laserSwitchOn_idcs(N):laserSwitchOff_idcs(N);
@@ -20,12 +21,13 @@ function batchExtractHOG_par(video_path, laserSwitchOn_idcs, laserSwitchOff_idcs
     hog_ChunkN = single.empty;
     for frame=1:length(recordedFrames)
         % read the frames and convert them into HOG vectors
-            img = read(vidReader, frame);
+            img = read(vidReader, recordedFrames(frame));
             img = grayCrop(img, pos_snout);
             hog_vec = extractHOGFeatures(img, 'CellSize', [32 32], 'NumBins', 8, ...
                                 'BlockSize', [1 1]);
             hog_ChunkN(end+1, :) = hog_vec;
     end
+        
         cossim_hogs = pdist(hog_ChunkN, 'cosine');
         links = linkage(cossim_hogs, 'average');
     
