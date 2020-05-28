288f953a9f78bcd468fe34c8fe85a57524ecef6d
wiessall <tristan.wiessalla@embl.de>
2020-05-28 09:47:37 +0200

    cleanup

--- postAnalysis/removeDuplicatePatterns.m
+++ postAnalysis/removeDuplicatePatterns.m
@@ -1,18 +1,19 @@
-function allPatterns = removeDuplicatePatterns(allPatterns, windowSize)
+function allPatterns = removeDuplicatePatterns(allPatterns)
 %% Remove all circular permutations of the activity patterns detected
     duplicateRows = [];
     for i=1:size(allPatterns, 1)
         % Duplicate the patterns to detect the circular permutations, see
         % https://www.geeksforgeeks.org/a-program-to-check-if-strings-are-rotations-of-each-other/?ref=rp
         testedRow = [allPatterns(i,:) allPatterns(i,:)];
-        for j=i+1:size(allPatterns, 1)
+        parfor j=i+1:size(allPatterns, 1)
             itrRow = allPatterns(j,:);
             if any(strfind(testedRow, itrRow))
                 % Store indices of duplicates
-                duplicateRows(end+1) = j;
+                duplicateRows(j,:) = j;
             end
         end
     end
+    duplicateRows(duplicateRows == 0) = [];
     % Pairs of duplicates are detected twice with the method above
     duplicateRows = unique(duplicateRows);
     % Remove the duplicate rows
