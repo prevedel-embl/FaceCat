898d08246b2b046c0e8cae96b2c0b2c4e8555fa6
countkrusty <tristan.wiessalla@posteo.de>
2020-09-21 11:30:31 +0200

    correct parser, correct logicals for while loop

--- optimizeMotE.m
+++ optimizeMotE.m
@@ -1,4 +1,4 @@
-function [score, classifiedFrames] = optimizeMotE(energy, varargin)
+function [scor, classifiedFrames] = optimizeMotE(energy, varargin)
 %% Find a cutoff value for the linkage of the HOG vectors that corresponds to the motion Energy calculated for the ROI.
 
 % Initialize values, find the threshold for the motion energy
@@ -6,14 +6,20 @@ function [score, classifiedFrames] = optimizeMotE(energy, varargin)
 
     parser = inputParser;
     validNoClusters = @(x) isnumeric(x) && isscalar(x);
-    validVector = @(x) isnumeric(x) && size(x, 2) == 1;
-    validLinkage = @(x) isnumeric(x) && size(x, 2) == 3;
+    validVector = @(x) isnumeric(x) && size(x, 1) == 1;
+%     validLinkage = @(x) isnumeric(x) && size(x, 2) == 3;
     addRequired(parser, 'energy', validVector);
-    addRequired(parser, 'links', validLinkage);
+    addRequired(parser, 'links');
     addParameter(parser, 'noClusters', defaultNoClusters, ...
                  validNoClusters);
     parse(parser, energy, varargin{:});
     
+    energy = parser.Results.energy;
+    links = parser.Results.links;
+    noClusters = parser.Results.noClusters;
+
+% Mean plus 2 sd is used to determine threshold for significant energy
+% between frames
     mu = mean(energy(:));
     sig = std(energy(:));
     threshold = mu + 2*sig;
@@ -21,21 +27,25 @@ function [score, classifiedFrames] = optimizeMotE(energy, varargin)
 % Create the logical vector indicating significant motion energy at each
 % frame
     bin_motE = energy > threshold;
-
+    scor = 0;
+    noClustersOld = NaN;
+   
     % Terminate either when the optimatl solution is found or if further
     % iterations don't change the outcome
-    while score ~= length(energy) || noClusters ~= noClustersOld
+    while scor ~= length(energy) & noClusters ~= noClustersOld
         classifiedFrames = cluster(links, 'maxclust', noClusters);
         % Find the logical motion Energy of the classifiedFrames
         clusterMotE = ischange(classifiedFrames);
         % Ideally both logical energy descriptions should match
-        score = sum(clusterMotE == bin_motE);
+        scor = sum(clusterMotE == bin_motE);
         noClustersOld = noClusters;
-        % Update noClusters to iteratively get cluser to
-        if length(find(clusterMotE == 1)) => length(find(bin_MotE))
+        % Update noClusters to iteratively get closer to
+        if length(find(clusterMotE == 1)) > length(find(bin_motE == 1))
             noClusters = floor(noClustersOld/2);
-        else
+        elseif length(find(clusterMotE == 1)) < length(find(bin_motE == 1))
             noClusters = floor(noClustersOld*1.5);
+        else
+            disp('noClusters did not change');
         end
     end
 end
